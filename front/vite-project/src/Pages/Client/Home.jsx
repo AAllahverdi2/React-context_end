b1d9b4baf36d99a350037abcc2ef2cca71eae01e
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2024-08-05 13:26:00 +0400

    lasted section added

--- front/vite-project/src/Pages/Client/Home.jsx
+++ front/vite-project/src/Pages/Client/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HomeSection1 from '../../companents/Client/HomeSection1'
 import Cards from '../../companents/Client/Cards'
+import Last from '../../companents/Client/Last'
 const Home = () => {
   return (
     <div>
         <HomeSection1/>
         <Cards/>
+        <Last/>
       
     </div>
   )
---
+++ front/vite-project/src/companents/Client/Last.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Last = () => {
+  return (
+    <div className='container' style={{marginTop:"20px"}}>
+        <div className="row">
+            <div className="col-lg-4 col-md-6 mb-3 mr-3" >
+                <img src="https://preview.colorlib.com/theme/coloshop/images/blog_1.jpg" alt="" />
+            </div>
+            <div className="col-lg-4 col-md-6 mb-3 mr-3" >
+<img src="https://preview.colorlib.com/theme/coloshop/images/blog_2.jpg" alt="" />
+            </div>
+
+            <div className="col-lg-4 col-md-6 mb-3 mr-3" >
+<img src="https://preview.colorlib.com/theme/coloshop/images/blog_3.jpg" alt="" />
+            </div>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default Last
